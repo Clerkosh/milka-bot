3b0933ac57a12436f31d5ef786b3417696380758
Tomek <wi101328@devtools.wi.pb.edu.pl>
2019-03-29 18:48:23 +0100

    dodanie server_port and server_host

--- bot.js
+++ bot.js
@@ -13,6 +13,11 @@ var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
